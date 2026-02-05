59c1db6724e21983bd59846ed513e792344517b0
tnt-nitro <91749046+tnt-nitro@users.noreply.github.com>
2026-02-05 19:59:06 +0100

    Entferne persÃ¶nliche Startskripte (start.bat, start.ps1) aus Repository und fÃ¼ge sie zur .gitignore hinzu

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "D:\Festplatte D\Programmieren\project_gpt_timestamp\script\gpt_time_stamp-1"
-if exist .venv\Scripts\activate.bat (
-    call .venv\Scripts\activate.bat
-)
-python src/gpt_time_stamp/app.py
-pause
